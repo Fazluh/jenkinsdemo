f45360414152da795b3ae25d13c13aa1b34d9627
Fazluh <fk368414@gmail.com>
2023-12-04 13:04:49 +0530

    add Product controller and product model

---
+++ jenkinsdemo/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using jenkinsdemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jenkinsdemo.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        // GET: Product
+        public ActionResult Index()
+
+        {
+            ProductModel m = new ProductModel();
+            m.ProdID = 1;
+            m.Prodname = "ABC";
+            return View();
+        }
+    }
+}
---
+++ jenkinsdemo/Models/ProductModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using jenkinsdemo.Models;
+
+namespace jenkinsdemo.Models
+{
+    public class ProductModel
+    {
+        public int ProdID { get; set; }
+
+    }
+}
